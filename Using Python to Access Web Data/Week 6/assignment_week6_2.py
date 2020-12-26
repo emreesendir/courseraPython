de02174d9e14c59ebba65a200307d7f88e88e4db
emreesendir <mkm.emre.esendir@gmail.com>
2020-12-26 18:32:35 +0100

    End of Using Python to Access Web Data Week 6

--- Using Python to Access Web Data/Week 6/assignment_week6_2.py
+++ Using Python to Access Web Data/Week 6/assignment_week6_2.py
@@ -2,7 +2,7 @@ import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
 
-# Ignore SSL certificate errors
+# Ignore SSL certificate error
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
